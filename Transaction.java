7b5ff3fca01ec9999a5b67b838f98c1a0f130792
rachna <rachnaoem07@gmail.com>
2025-02-12 08:41:59 +0700

    edit(book and transaction) about class and obj

--- Transaction.java
+++ Transaction.java
@@ -1,18 +1,22 @@
-// import java.util.Date;
+import java.util.Date;
 public class Transaction {
     int transactionId;
     int userId;
     int bookId;
-    // Date borrow_Date;
-    // Date dueDate;
-    // Date returnDate;
-    Transaction(int transactionId, int userId, int bookId) {
+    Date borrowDate;
+    Date dueDate;
+    Date returnDate;
+
+    Transaction(int transactionId, int userId, int bookId, Date borrowDate) {
         this.transactionId = transactionId;
         this.userId = userId;
         this.bookId = bookId;
-        // borrowDate = borrow_Date;
-        // dueDate = calculateDueDate(borrowDate, 14); 
-        // returnDate = null; 
+        this.borrowDate = borrowDate;
+
+        // Add 14 days to borrowDate to calculate dueDate
+        this.dueDate = new Date(borrowDate.getTime() + (14L * 24 * 60 * 60 * 1000)); // 14 days in milliseconds
+
+        this.returnDate = null; // Initially, returnDate is null
     }
 
 }
