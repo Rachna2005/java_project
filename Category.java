1a42d2cd5363831d536673d6d62bc9e47e2b6168
sutheavy22 <sutheavy22@gmail.com>
2025-02-19 19:21:10 +0700

    add library class

--- Category.java
+++ Category.java
@@ -1,4 +1,29 @@
 public class Category {
+    private static int categoryIdCounter = 0;
+    private int categoryId;
+    private String name;
+    private String description;
 
-    
+    public Category(String name, String description) {
+        this.categoryId = ++categoryIdCounter;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category ID: " + categoryId + ", Name: " + name + ", Description: " + description;
+    }
 }
---
+++ Library.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Author> authors;
+    private List<Book> books;
+    private List<Category> categories;
+    private List<Transaction> transactions;
+
+    public Library() {
+        authors = new ArrayList<>();
+        books = new ArrayList<>();
+        categories = new ArrayList<>();
+        transactions = new ArrayList<>();
+    }
+
+    // Add an author
+    public void addAuthor(Author author) {
+        authors.add(author);
+    }
+
+    // Get all authors
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    // Add a book
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    // Get all books
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    // Add a category
+    public void addCategory(Category category) {
+        categories.add(category);
+    }
+
+    // Get all categories
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    // Add a transaction
+    public void borrowBook(int userId, int bookId) {
+        Book book = findBookById(bookId);
+        if (book != null && book.numOfBook > 0) {
+            book.numOfBook--; // Reduce available book count
+            transactions.add(new Transaction(userId, bookId, new Date()));
+
+        } else {
+            System.out.println("Book not available");
+        }
+    }
+
+    // Find a book by ID
+    private Book findBookById(int bookId) {
+        for (Book book : books) {
+            if (book.id == bookId) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    // Display all transactions
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
